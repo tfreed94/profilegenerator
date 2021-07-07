8f4786ddaee9113f400d58236e6924c7d36a088f
Troy Freed <tfreed94@gmail.com>
2021-07-06 20:51:44 -0400

    trying to fix error with writing the file how i want it

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const fs = require("fs");
 const arrayOfIDs = [];
-const render = require("./src/generate.js");
+const generate = require("./src/generate.js");
 const team = [];
 
 
@@ -18,42 +18,114 @@ mainPage = () => {
             .prompt([
                 {
                     type: "input",
-                    name: "managerName",
+                    name: "nameOfManager",
                     message: "Enter name of team manager",
                 },
                 {
                     type: "input",
-                    name: "managerId",
+                    name: "idForManager",
                     message: "What's the managers id?",
                 },
                 {
                     type: "input",
-                    name: "managerEmail",
+                    name: "emailForManager",
                     message: "Email for team manager?",
                 },
                 {
                     type: "input",
-                    name: "managerOfficeNumber",
+                    name: "offNumManager",
                     message: "Office number for team manager",
                 }
             ]).then(answers => {
                 const manager = new Manager(
-                    answers.managerName,
-                    answers.managerId,
-                    answers.managerEmail,
-                    answers.managerOfficeNumber);
+                    answers.nameOfManager,
+                    answers.idForManager,
+                    answers.emailForManager,
+                    answers.offNumManager);
                 team.push(manager);
-                arrayOfIDs.push(answers.managerId);
+                arrayOfIDs.push(answers.idForManager);
                 teamCreate();
             });
     }
-    // Use inquirer to ask which user what kind of member they want to add next
+    // Use inquirer to prompt user with intern questions
+    createIntern = () => {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "nameOfIntern",
+                    message: "Name of intern?",
+                },
+                {
+                    type: "input",
+                    name: "idForIntern",
+                    message: "Intern ID?",
+                },
+                {
+                    type: "input",
+                    name: "emailForIntern",
+                    message: "Email for intern?",
+                },
+                {
+                    type: "input",
+                    name: "schoolOfIntern",
+                    message: "What school did your intern attend?",
+                }
+            ]).then(answers => {
+                const intern = new Intern(
+                    answers.nameOfIntern,
+                    answers.idForIntern,
+                    answers.emailForIntern,
+                    answers.schoolOfIntern);
+                team.push(intern);
+                arrayOfIDs.push(answers.idForIntern);
+                teamCreate();
+            });
+    }
+
+    // Use inquirer to promp user with engineer questions
+    createEngineer = () => {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "nameOfEng",
+                    message: "Name of engineer?",
+                },
+                {
+                    type: "input",
+                    name: "idForEng",
+                    message: "Engineer ID?",
+                },
+                {
+                    type: "input",
+                    name: "emailForEng",
+                    message: "Email for engineer?",
+                },
+                {
+                    type: "input",
+                    name: "engGH",
+                    message: "Github for engineer?",
+                }
+            ]).then(answers => {
+                const engineer = new Engineer(
+                    answers.nameOfEng,
+                    answers.idForEng,
+                    answers.emailForEng,
+                    answers.engGH);
+                team.push(engineer);
+                arrayOfIDs.push(answers.idForEng);
+                teamCreate();
+            });
+    }
+
+    // Use inquirer to ask which user what kind of member they want to add next if they want to
     teamCreate = () => {
         inquirer
             .prompt([
                 {
                     type: "list",
-                    name: "memberChoice",
+                    name: "addNewMem",
                     message: "What kind of team member do you want to add next?",
                     choices:
                         [
@@ -63,7 +135,7 @@ mainPage = () => {
                         ]
                 }
             ]).then(userChoice => {
-                switch (userChoice.memberChoice) {
+                switch (userChoice.addNewMem) {
                     case "Engineer":
                         createEngineer();
                         break;
@@ -75,82 +147,11 @@ mainPage = () => {
                 }
             });
     }
-    // Use inquirer to promp user with engineer questions
-    createEngineer = () => {
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    name: "engineerName",
-                    message: "Name of engineer?",
-                },
-                {
-                    type: "input",
-                    name: "engineerId",
-                    message: "Engineer ID?",
-                },
-                {
-                    type: "input",
-                    name: "engineerEmail",
-                    message: "Email for engineer?",
-                },
-                {
-                    type: "input",
-                    name: "engineerGithub",
-                    message: "Github for engineer?",
-                }
-            ]).then(answers => {
-                const engineer = new Engineer(
-                    answers.engineerName,
-                    answers.engineerId,
-                    answers.engineerEmail,
-                    answers.engineerGithub);
-                team.push(engineer);
-                arrayOfIDs.push(answers.engineerId);
-                teamCreate();
-            });
-    }
-    // Use inquirer to prompt user with intern questions
-    createIntern = () => {
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    name: "internName",
-                    message: "Name of intern?",
-                },
-                {
-                    type: "input",
-                    name: "internId",
-                    message: "Intern ID?",
-                },
-                {
-                    type: "input",
-                    name: "internEmail",
-                    message: "Email for intern?",
-                },
-                {
-                    type: "input",
-                    name: "internSchool",
-                    message: "What school did your intern attend?",
-                }
-            ]).then(answers => {
-                const intern = new Intern(
-                    answers.internName,
-                    answers.internId,
-                    answers.internEmail,
-                    answers.internSchool);
-                team.push(intern);
-                arrayOfIDs.push(answers.internId);
-                teamCreate();
-            });
-    }
-
     generateTeam = () => {
         if (!fs.existsSync(OUTPUT_DIR)) {
             fs.mkdirSync(OUTPUT_DIR)
         }
-        fs.writeFileSync(outputPath, render(team), "utf-8");
+        fs.writeFileSync(outputPath, generate(team), "utf-8");
     }
     setManager();
 
--- src/generate.js
+++ src/generate.js
@@ -2,17 +2,14 @@
 const teamGenerator = team => {
     const managerTemplate = manager => {
         return `
-        <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            <h3 class="card-title">${manager.getRole()}</h3>
         </div>
         <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
+            <p>ID: ${manager.getId()}</p>
+            <p>Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+            <p>Office number: ${manager.getOfficeNumber()}</p>
         </div>
     </div>
         `;
@@ -21,17 +18,14 @@ const teamGenerator = team => {
     // Template Literal for engineer
     const engineerTemplate = engineer => {
         return `
-        <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <h3 class="card-title">${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
+        <p>ID: ${engineer.getId()}</p>
+        <p>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+        <p>GitHub: <a href="https://github.com/${engineer.getGithub()}>${engineer.getGithub()}</a></p>
     </div>
 </div>
         `;
@@ -40,17 +34,14 @@ const teamGenerator = team => {
     // Template Literal for intern
     const internTemplate = intern => {
         return `
-        <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        <h3 class="card-title">${intern.getRole()}</h3>
     </div>
     <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
+        <p>ID: ${intern.getId()}</p>
+        <p>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+        <p>School: ${intern.getSchool()}</p>
     </div>
 </div>
         `;
@@ -78,20 +69,19 @@ module.exports = team => {
     <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
 <body>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+        <div>
+            <div class="col-12 mb-3 team-heading">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+    <div class="container-fluid">
+        <div>
+            <div col-12 d-flex justify-content-center">
                 ${teamGenerator(team)}
             </div>
         </div>
